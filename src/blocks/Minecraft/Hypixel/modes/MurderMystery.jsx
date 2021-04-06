bc046843794351b564b823f9f79f4574aee37b65
Пронин Егор <mrzillagold@gmail.com>
2021-04-06 23:05:39 +1000

    Исправление оступов

--- src/blocks/Minecraft/Hypixel/modes/MurderMystery.jsx
+++ src/blocks/Minecraft/Hypixel/modes/MurderMystery.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Cell } from "@vkontakte/vkui";
+import { SimpleCell } from "@vkontakte/vkui";
 
 import { getRatio } from "../../../../functions";
 
@@ -10,45 +10,45 @@ export function MurderMystery({ user }) {
     return (
         <div>
             <div className="Info-List">
-                <Cell description="Монет"
+                <SimpleCell description="Монет"
                       disabled
                 >
                     { MurderMystery.coins.toLocaleString() }
-                </Cell>
+                </SimpleCell>
             </div>
             <div className="Info-List">
-                <Cell description="Побед"
+                <SimpleCell description="Побед"
                       disabled
                 >
                     { MurderMystery.wins.toLocaleString() }
-                </Cell>
-                <Cell description="Поражений"
+                </SimpleCell>
+                <SimpleCell description="Поражений"
                       disabled
                 >
                     { (MurderMystery.games_played - MurderMystery.wins).toLocaleString() }
-                </Cell>
-                <Cell description="П/П"
+                </SimpleCell>
+                <SimpleCell description="П/П"
                       disabled
                 >
                     { getRatio(MurderMystery.wins, MurderMystery.games_played - MurderMystery.wins) }
-                </Cell>
+                </SimpleCell>
             </div>
             <div className="Info-List">
-                <Cell description="Убийств"
+                <SimpleCell description="Убийств"
                       disabled
                 >
                     { MurderMystery.kills.toLocaleString() }
-                </Cell>
-                <Cell description="Смертей"
+                </SimpleCell>
+                <SimpleCell description="Смертей"
                       disabled
                 >
                     { MurderMystery.deaths.toLocaleString() }
-                </Cell>
-                <Cell description="У/С"
+                </SimpleCell>
+                <SimpleCell description="У/С"
                       disabled
                 >
                     { MurderMystery.kill_death_ratio }
-                </Cell>
+                </SimpleCell>
             </div>
         </div>
     )
